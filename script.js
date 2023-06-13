8e16f03b34789e5e85971a0f371722b3cf6f4d7b
Sascha Keweloh <sascha.keweloh@tu-dortmund.de>
2023-06-13 14:04:55 +0200

    no menu

--- script.js
+++ script.js
@@ -1,39 +1,5 @@
-// Add a class to the menu when scrolling
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    const menu = document.querySelector('.menu');
-    const headerHeight = header.offsetHeight;
-  
-    if (window.scrollY > headerHeight) {
-        menu.classList.add('fixed-menu');
-    } else {
-        menu.classList.remove('fixed-menu');
-    }
+$(document).ready(function() {
+  $('.abstract-toggle').click(function() {
+    $(this).closest('li').find('.abstract').toggle();
+  });
 });
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var menuToggle = document.querySelector('.menu-toggle');
-    var menuItems = document.querySelector('.menu');
-    
-    menuToggle.addEventListener('click', function() {
-      menuItems.classList.toggle('collapsible');
-    });
-  });
-  
-  document.addEventListener("DOMContentLoaded", function() {
-    const abstractToggleButtons = document.querySelectorAll(".abstract-toggle");
-  
-    abstractToggleButtons.forEach(function(button) {
-      button.addEventListener("click", function(event) {
-        event.preventDefault(); // Prevent the default anchor behavior
-  
-        const abstract = this.nextElementSibling;
-        abstract.classList.toggle("show");
-      });
-    });
-  });
-  
-  
-  
-  
