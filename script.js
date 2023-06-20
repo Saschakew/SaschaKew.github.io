23b34b6f4985f3c9a6e0f45a79016ff1d7c2b1b7
Sascha Keweloh <sascha.keweloh@tu-dortmund.de>
2023-06-20 13:52:45 +0200

    SVARpy collapse

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ $(document).ready(function() {
     $(this).siblings('.abstract').toggle();
   });
 });
+
+// Get the expand/collapse button
+const expandCollapseButton = document.getElementById("expand-collapse");
+
+// Get the list container
+const listContainer = document.getElementById("notebook-list-container");
+ 
+
+// Add event listener to the expand/collapse button
+expandCollapseButton.addEventListener("click", () => {
+  // Toggle the class for the list container
+  listContainer.classList.toggle("collapsed");
+
+  expandCollapseButton.classList.toggle("expanded");
+});
